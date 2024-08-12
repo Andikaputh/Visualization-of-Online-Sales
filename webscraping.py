c99e4e356469f5b53be0871bbb1a7a126a40e0ff
Andika Putra Perdana <andikaputh1@gmail.com>
2024-08-12 17:02:20 +0700

    modify webscarping

--- webscraping.py
+++ webscraping.py
@@ -2,8 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-# URL halaman e-commerce yang ingin di-scrape
-url = 'https://books.toscrape.com/'
+# URL halaman yang ingin di-scrape
+url = 'http://books.toscrape.com/'
 
 # Mengambil konten halaman web
 response = requests.get(url)
@@ -11,19 +11,17 @@ soup = BeautifulSoup(response.content, 'html.parser')
 
 # Menentukan elemen HTML yang berisi data
 data = []
-table = soup.find('table', {'id': 'sales-data-table'})  # Ganti dengan elemen yang sesuai
-rows = table.find_all('tr')
+books = soup.find_all('article', class_='product_pod')
 
-# Mengambil header
-headers = [header.text for header in rows[0].find_all('th')]
+# Mengambil data buku
+for book in books:
+    title = book.h3.a['title']
+    price = book.select_one('p.price_color').text
+    rating = book.p['class'][1]
+    data.append([title, price, rating])
 
-# Mengambil baris data
-for row in rows[1:]:
-    cols = row.find_all('td')
-    data.append([col.text for col in cols])
-
-# Menyimpan data ke CSV
-df = pd.DataFrame(data, columns=headers)
+# Menyimpan data ke CSV dengan nama file yang baru
+df = pd.DataFrame(data, columns=['Title', 'Price', 'Rating'])
 df.to_csv('data_penjualan.csv', index=False)
 
 print("Data telah disimpan ke data_penjualan.csv")
